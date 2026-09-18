4f7271a0a542001616d32834d8c46ccfc595dad6
Alphie <kipsangalfred237@gmail.com>
2026-09-18 12:07:58 +0300

    Configure server port for deployment

--- server/index.js
+++ server/index.js
@@ -60,6 +60,8 @@ app.delete('/api/snippets/:id', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
